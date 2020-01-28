164cd7d26fd14d1e2e6998eca0c325a37e19dd55
unknown <hubertgiang5@gmailcom>
2020-01-28 01:38:56 -0500

    lab02 part 2

--- lab02/build/scripts/lab02.bat
+++ lab02/build/scripts/lab02.bat
@@ -79,7 +79,7 @@ set CMD_LINE_ARGS=%*
 :execute
 @rem Setup the command line
 
-set CLASSPATH=%APP_HOME%\lib\lab02.jar
+set CLASSPATH=%APP_HOME%\lib\lab02.jar;%APP_HOME%\lib\commons-csv-1.7.jar
 
 @rem Execute lab02
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %LAB02_OPTS%  -classpath "%CLASSPATH%" SimpleTest %CMD_LINE_ARGS%
--- lab02/src/main/java/csci2020u/lab02/SimpleTest.java
+++ lab02/src/main/java/csci2020u/lab02/SimpleTest.java
@@ -1,6 +1,6 @@
 
 public class SimpleTest {
 	 public static void main(String[] args) {
-	System.out.println("Hello World, I am here!");
+	System.out.println("Hello World, I am here! This is lab 02!");
 	 }
 }
